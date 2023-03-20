0f585ce537080fd66e53ed7a9e871a0409dc8573
Ignas Galvelis <igalvelis@gmail.com>
2023-03-20 23:55:25 +0000

    Implement landing page contents

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 //import styles from '@/styles/Home.module.css'
+import { Button, Card, CardContent, Typography } from '@mui/material';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,22 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
+          <Card sx={{ maxWidth: 500 }}>
+              <CardContent>
+                  <Typography variant="h4" component="h1" gutterBottom>
+                      Mint your thoughts into an NFT
+                  </Typography>
+                  <Typography variant="body1" component="p" gutterBottom>
+                      Upload your faults and mint them into NFTs. Claim ownership and sell to others.
+                  </Typography>
+                  <Typography variant="body1" component="p" gutterBottom>
+                      Build a model that can recognize faults and control your computer without using your hands.
+                  </Typography>
+                  <Button variant="contained" color="primary">
+                      Mint Now
+                  </Button>
+              </CardContent>
+          </Card>
       </main>
     </>
   )
